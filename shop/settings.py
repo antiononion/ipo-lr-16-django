79dc331355a94fb8a5b01594e1956b513e21a331
Anton <antiononion@gmail.com>
2026-06-17 02:11:26 +0300

    Add files via upload

--- shop/settings.py
+++ shop/settings.py
@@ -12,6 +12,8 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',')
+
 INSTALLED_APPS = [      
     'django.contrib.admin',
     'django.contrib.auth',
